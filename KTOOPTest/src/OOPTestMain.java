13b06e1a8b37fbbb8d325fc37ab8e93f5ee3b07a
Admin-PC\Admin <bluebus4727@gmail.com>
2017-01-23 15:33:01 +0900

    no message

---
+++ KTOOPTest/src/OOPTestMain.java
@@ -0,0 +1,23 @@
+
+public class OOPTestMain {
+	
+	public static void main(String[] args) {
+		
+		 OOPTset stand = new OOPTset();
+		 
+		 stand.brightnessControlUp();
+		 stand.brihtnessControlDown();
+		 
+		 stand.on();
+		 
+		 stand.brightnessControlUp();
+		 stand.brihtnessControlDown();
+		 
+		 stand.off();
+	
+		 
+		 
+		
+	}
+
+}
