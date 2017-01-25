be6f39291b61eda26dccda925e6fc8311d607ac7
Admin-PC\Admin <bluebus4727@gmail.com>
2017-01-25 15:34:42 +0900

    no message

---
+++ Recursion/src/com/ktds/jjh/Main.java
@@ -0,0 +1,5 @@
+package com.ktds.jjh;
+
+public class Main {
+
+}
---
+++ Recursion/src/com/ktds/jjh/Recursion.java
@@ -0,0 +1,10 @@
+package com.ktds.jjh;
+
+public class Recursion {
+	
+	public void call() {
+		System.out.println("Recursion.call() 호출 중....");
+		
+	}
+
+}
